bec0753d0d46ec0ace9939488c4d5f98d29c53f5
VadoSevich <checherinda.vadym@gmail.com>
2017-08-20 01:13:10 +0300

    More faster version

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,44 @@ var gulp         = require('gulp'),
 	pngquant     = require('imagemin-pngquant'),
 	cache        = require('gulp-cache'),
 	sourcemaps   = require('gulp-sourcemaps'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	htmlbuild    = require('gulp-htmlbuild');
+
+
+var path = {
+	src: {
+		libsCss: ['src/scss/libs.scss'],
+		libsJs: [
+			'src/libs/jquery/dist/jquery.min.js', // For js plugins
+			'src/libs/slick-carousel/slick/slick.js',
+		],
+		html: ['src/*.html'],
+		scss: ['src/scss/*.scss','!src/scss/libs.scss'],
+		js: ['src/js/**/*.js','!src/js/**/*.min.js']
+	},
+	build: {
+		filesCss: [
+			'src/css/libs.min.css',
+			'src/css/style.min.css'
+		],
+		filesJs: [
+			'src/js/minify/libs.min.js',
+			'src/js/minify/scripts.min.js'
+		],
+		fonts: ['src/fonts/**/*']
+	}
+}
+
+gulp.task('css-libs', function(){
+	return gulp.src(path.src.libsCss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('src/css'))
+});
 
 gulp.task('sass', function(){ 
-	return gulp.src('src/scss/*.scss') //Input scss files
+	return gulp.src(path.src.scss) //Input scss files
 		.pipe(sourcemaps.init())
 			.pipe(sass().on('error', sass.logError)) // Scss into css files
 			.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Vendor prefixes
@@ -36,14 +70,15 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-const path = [
-	'src/libs/jquery/dist/jquery.min.js',
-	'src/libs/slick-carousel/slick/slick.js',
-	'src/js/common.js'
-]
+gulp.task('js-libs', function(){
+	return gulp.src(path.src.libsJs)
+		.pipe(uglify())
+		.pipe(concat('libs.min.js'))
+		.pipe(gulp.dest('src/js/minify'))
+});
 
 gulp.task('scripts', function() { 
-	return gulp.src(path)
+	return gulp.src('src/js/*.js')
 		.pipe(sourcemaps.init())
 			.pipe(babel({
 				presets: ['env']
@@ -59,18 +94,22 @@ gulp.task('scripts', function() {
 			.pipe(uglify()) // compress JS file
 			.pipe(concat('scripts.min.js')) // concatination in new file scripts.min.js
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('src/js')) // Output folder
+		.pipe(gulp.dest('src/js/minify')) // Output folder
 		.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('watch', ['browser-sync','sass','scripts'], function() {
-	gulp.watch('src/scss/**/*.scss', ['sass']); // watching for scss files
-	gulp.watch(['src/js/**/*.js','!src/js/**/*.min.js'],['scripts']);   // watching for js files
-	gulp.watch('src/*.html', browserSync.reload); // watching for html files
+gulp.task('watch', ['browser-sync','css-libs','sass','js-libs','scripts'], function() {
+	gulp.watch(path.src.scss, ['sass']); // watching for scss files
+	gulp.watch(path.src.js,['scripts']);   // watching for js files
+	gulp.watch(path.src.html, browserSync.reload); // watching for html files
 });
 
+
+// DIST mode
+
+
 gulp.task('clean', function() {
-	return del.sync('dist'); // Delete dist folder before build
+	return del.sync(['dist/**', '!dist'], {force:true}); // Delete dist folder before build
 });
 
 gulp.task('img', function() {
@@ -84,25 +123,42 @@ gulp.task('img', function() {
 		.pipe(gulp.dest('dist/img')); // Output dist folder
 });
 
-gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+gulp.task('js-build', function(){
+	return gulp.src(path.build.filesJs)
+		.pipe(concat('scripts.min.js'))
+		.pipe(gulp.dest('./dist/js'))
+});
 
-	var buildCss = gulp.src('src/css/style.min.css')
-	.pipe(gulp.dest('dist/css'))
+gulp.task('css-build', function(){
+	return gulp.src(path.build.filesCss)
+		.pipe(concat('styles.min.css'))
+		.pipe(gulp.dest('./dist/css'))
+});
 
-	var buildJs = gulp.src('src/js/scripts.min.js') // Output js in dist folder
-	.pipe(gulp.dest('dist/js'))
-
-	var buildFonts = gulp.src('src/fonts/**/*') // Output fonts in dist folder
-	.pipe(gulp.dest('dist/fonts'))
-
-
-	var buildHtml = gulp.src('src/*.html') // Output html in dist folder
-	.pipe(gulp.dest('dist'));
-
+gulp.task('html', function(){
+	return gulp.src(['./src/*.html'])
+		.pipe(htmlbuild({
+			js: htmlbuild.preprocess.js(function (block) {
+				block.write('js/scripts.min.js');
+        		block.end();
+			}),
+			css: htmlbuild.preprocess.css(function (block) {
+				block.write('css/styles.min.css');
+        		block.end();
+			})
+		}))
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('clear', function (callback) {
 	return cache.clearAll();
 })
+
+gulp.task('build', ['clean','img','css-build','js-build','html'], function() {
+
+	var buildFonts = gulp.src(path.build.fonts) 
+	.pipe(gulp.dest('dist/fonts'));
+
+});
 
 gulp.task('default', ['watch']);
--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,28 @@
+'use strct'
+
 $(function() {
 
- var $header = $('h1');
+	(function(d, s, id) {
+	  var js,
+	      fjs = d.getElementsByTagName(s)[0],
+	      t = window.twttr || {};
 
- $header.css('color','blue');
+	  var src= [
+	  	  	"https://platform.twitter.com/widgets.js",
+	  	  	"https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.10"
+	  	  ];
+
+	  if (d.getElementById(id)) return t;
+
+	  	  for(var i = 0; i < id.length; i++) {
+			  js = d.createElement(s);
+			  js.id = id[i];
+			  js.src = src[i];
+			  fjs.parentNode.insertBefore(js, fjs);
+	  	  }
+
+	  return t;
+	  
+	}(document, "script", ["twitter-wjs","facebook-jssdk"]));
 
 });
